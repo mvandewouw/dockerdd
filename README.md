551b4c75d5db6cdb0da81e22fa56213d1c0cd7fd
Ernest Neijenhuis PA3HCM <ernest@utreg.net>
2022-12-29 10:13:58 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,92 @@
+# Dockerdd
+
+Dockerdd is a simple Python script to replicate Docker images between
+registries.
+
+
+## Why Dockerdd
+
+If you have multiple registries and want to keep some images in sync, this
+script might be your solution. You simply describe the sync in a yaml file and
+run the script.
+
+
+## Installation
+
+To run this script, you will need:
+
+- Python (including pip)
+- Docker
+- Python's Docker library
+
+To install the Docker library for Python, run the following command:
+
+``` sh
+pip install docker
+```
+
+
+## Create the jobs file
+
+This is the structure of a jobs file, it's in yaml format:
+
+``` yaml
+registries:
+  <registry-name>:
+    address: <registry url without the 'http(s)://', or use 'default'>
+  <registry-name-2>:
+    address: ...
+
+imagelists:
+  <imagelist-name>:
+    - image1:tag
+    - image2:tag
+    - image3:tag
+  <imagelist-name-2>:
+    - ...
+
+jobs:
+  <job-name>:
+    source: <source registry-name>
+    target: <target registry-name>
+    imagelists:
+      - <imagelist-name-1>
+      - <imagelist-name-2>
+      - ...
+  <job-name-2>:
+    source: ...
+    ...
+```
+
+First define the registries you want to use. Define the registry's address,
+e.g. `harbor.mydomain.com/dev-images`. You can also use 'default', in that case
+Docker will use the default registry as defined by the Docker configuration.
+
+Next define imagelists. Each imagelist has a name and contains a list of images
+(with tags).
+
+Finally defines jobs. Each job copies the given imagelists from the given
+source registry to the target registry.
+
+You can define multiple jobs and imagelists. Jobs will be performed in the
+given order.
+
+
+## Run the script
+
+``` sh
+dockerdd.py <jobs-file>
+```
+
+
+## Authentication
+
+Some registries require authentication, this is not handled by Dockerdd. You
+can authenticate once (manually) using `docker login`, the secrets will be
+stored in `~/.docker/config.json`.
+
+
+## Gitlab pipeline
+
+The file `example-gitlab-ci.yml` demonstrates you how you can run this script
+in a pipeline.
---
+++ dockerdd.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+import argparse
+import docker
+import os
+import sys
+import time
+import yaml
+
+# Setup local docker client connection
+dockerclient = docker.from_env()
+
+# Import yaml file
+if len(sys.argv) > 0:
+    job_file = sys.argv[1]
+else:
+    job_file = 'jobs.yaml'
+
+from yaml.loader import SafeLoader
+with open(job_file, 'r') as f:
+    dddata = yaml.load(f, Loader=SafeLoader)
+
+# Process jobs
+for job in dddata['jobs']:
+    print(f"Job: {job}")
+
+    # Process imagelists for given job
+    for imagelist in dddata['jobs'][job]['imagelists']:
+        source = dddata['jobs'][job]['source']
+        target = dddata['jobs'][job]['target']
+        source_addr = dddata['registries'][source]['address']
+        target_addr = dddata['registries'][target]['address']
+        print(f"- Sync imagelist {imagelist} from {source} to {target}")
+
+        # Process images in given imagelist
+        for image in dddata['imagelists'][imagelist]:
+            result = 'ok'
+            image_name, image_tag = image.split(':')
+            if source_addr == 'default':
+                source_full = image_name + ':' + image_tag
+            else:
+                source_full = source_addr + '/' + image_name + ':' + image_tag
+            target_full = target_addr + '/' + image_name + ':' + image_tag
+
+            print(f"  - {image_name}:{image_tag}: ", end='', flush=True)
+
+            # Check if image is already in target registry
+            osout = os.system(f"docker manifest inspect {target_full} > /dev/null 2>&1")
+            if osout == 0:
+                print("cached... ok")
+            else:
+                # Pull image from source registry
+                print(f"pull... ", end='', flush=True)
+                try:
+                    dockerimage = dockerclient.images.pull(source_full)
+                except Exception as e:
+                    print(f"FAIL: {e}")
+                    if 'dockerimage' in locals():
+                        del dockerimage
+                    pass
+    
+                # Tag new registry
+                if 'dockerimage' in locals():
+                    print("tag... ", end='', flush=True)
+                    try:
+                        dockerimage.tag(target_full)
+                    except Exception as e:
+                        print(f"FAIL: {e}")
+                        dockerclient.images.remove(source_full)
+                        if 'dockerimage' in locals():
+                            del dockerimage
+                        pass
+    
+                # Push image to target registry
+                if 'dockerimage' in locals():
+                    print("push... ", end='', flush=True)
+                    try:
+                        pushed = dockerclient.images.push(repository=target_full)
+                        # Cleanup target image (tag)
+                        print("untag... ", end='', flush=True)
+                        dockerclient.images.remove(target_full)
+                    except Exception as e:
+                        print(f"FAIL: {e}")
+                        pass
+    
+                    # Check if image is available in target repo
+                    print("verify:", end='', flush=True)
+                    osout = os.system(f"docker manifest inspect {target_full} >/dev/null 2>/dev/null")
+                    if osout != 0:
+                        print("error... ", end='', flush=True)
+                        result = 'FAIL'
+                    else:
+                        print("ok... ", end='', flush=True)
+
+                    # Cleanup source image
+                    print("cleanup... ", end='', flush=True)
+                    try:
+                        dockerclient.images.remove(source_full)
+                    except:
+                        pass
+                    print(result)
